c67639d15a78d2f000bcf71ac230b23de199fcf7
kf0830s <kf0830s@gmail.com>
2020-10-15 08:47:14 +0900

    Add: functions and objects050

--- 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -1,7 +1,18 @@
+window.name = 'John';
+
 const person = {
     name: 'Tom',
     hello: function() {
         console.log('Hello ' + this.name);
+        a();
+
+        const person = {
+            name: 'Tim',
+            hello: function() {
+                console.log('Hello ' + this.name);
+                a();
+            }
+        }
+        person.hello();
     }
 }
-person.hello();
