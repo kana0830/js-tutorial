a8d02f4a43bf427fa78de611edf6e8727f861e9c
kf0830s <kf0830s@gmail.com>
2020-11-14 08:24:39 +0900

    Add: functions and objects2 920

--- 060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
+++ 060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
@@ -10,14 +10,17 @@
  */
 class Person {
   constructor(name, age) {
-      this.name = name;
-      this.age = age;
+    this.name = name;
+    this.age = age;
   }
 
   hello() {
-      console.log('hello ' + this.name);
+    console.log('hello ' + this.name);
   }
 }
 
 const bob = new Person('Bob', 23);
-setTimeout(bob.hello, 1000);
+setTimeout(Person.prototype.hello.bind(bob), 1000);
+setTimeout(function() {
+  bob.hello();
+} , 1000);
